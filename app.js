5b541cd29a9616b816fcab482f4f40e83cd0bf61
Albert Chen <alb0rt@users.noreply.github.com>
2015-01-12 20:58:32 -0800

    unbreaking click behavior

--- app.js
+++ app.js
@@ -1,61 +1,50 @@
+// Set up server
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var methodOverride = require('method-override');
 
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Set up mongoose
+var mongoose = require('mongoose');
+
+if (app.get('env') === 'development') {
+    mongoose.connect('mongodb://localhost:27017/county-log');
+    console.log("Connecting to Test Mongo");
+} else {
+    //mongoose.connect('mongodb://skronch:qwe123@ds035270.mongolab.com:35270/heroku_app29348857');
+    console.log("Connecting to Prod Mongo");
+}
+
 
 app.use(express.static(path.join(__dirname, 'html')));
-// uncomment after placing your favicon in /public
+app.use(express.static(path.join(__dirname, 'public')));
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-/*
-app.use('/', routes);
-app.use('/users', users);*/
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride());
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.listen(8080);
+console.log("App listening on port 8080");
+
+// Model for county list
+
+var CountyList = mongoose.model("CountyLog", {
+    id : String,
+    name : String
 });
 
-// error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+// API Routes
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+
 
 module.exports = app;
+
