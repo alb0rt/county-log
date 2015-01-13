2bc184a2a3b40df6c792d3c6bc31b8152d0a4165
Albert Chen <alb0rt@users.noreply.github.com>
2015-01-12 21:03:36 -0800

    removed mongo for now

--- app.js
+++ app.js
@@ -10,16 +10,6 @@ var methodOverride = require('method-override');
 
 var app = express();
 
-// Set up mongoose
-var mongoose = require('mongoose');
-
-if (app.get('env') === 'development') {
-    mongoose.connect('mongodb://localhost:27017/county-log');
-    console.log("Connecting to Test Mongo");
-} else {
-    //mongoose.connect('mongodb://skronch:qwe123@ds035270.mongolab.com:35270/heroku_app29348857');
-    console.log("Connecting to Prod Mongo");
-}
 
 
 app.use(express.static(path.join(__dirname, 'html')));
@@ -34,15 +24,6 @@ app.use(methodOverride());
 app.listen(8080);
 console.log("App listening on port 8080");
 
-// Model for county list
-
-var CountyList = mongoose.model("CountyLog", {
-    id : String,
-    name : String
-});
-
-
-// API Routes
 
 
 
